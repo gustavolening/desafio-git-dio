9a821ec274e857af42bc5f720f08d67b941498e8
gustavolening <gustavo.-godoy@hotmail.com>
2021-08-04 14:06:36 -0300

    adding eclipse folder

---
+++ ide-eclipse/ide-eclipse/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,49 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		gato.setCor("azul");
+		gato.setNome("azulão");
+		gato.setIdade(24);
+		livros.setNome("O rei demonio");
+		livros.setNumPaginas(350);
+		int a = 3;
+		int b = 5;
+
+		System.out.println("A soma é: " + (a + b));
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+
+}
+
+class Livros{
+	private String nome;
+	private Integer numPaginas;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getNumPaginas() {
+		return numPaginas;
+	}
+	public void setNumPaginas(Integer numPaginas) {
+		this.numPaginas = numPaginas;
+	}
+	@Override
+	public String toString() {
+		return "Livros [nome=" + nome + ", numPaginas=" + numPaginas + "]";
+	}
+	
+	
+	
+}
